9ecd854ff15a0c9ea009bb7ae7a0cf61c4f252ad
pelle <apellegrino@ns1.com>
2022-09-27 11:00:38 -0600

    Netbox Orb setup

---
+++ netbox_orb/__init__.py
@@ -0,0 +1,12 @@
+from extras.plugins import PluginConfig
+
+
+class NetBoxOrbConfig(PluginConfig):
+    name = 'netbox_orb'
+    verbose_name = ' NetBox Orb'
+    description = 'Mange Orb assets in NetBox'
+    version = '0.1'
+    base_url = 'orb'
+
+
+config = NetBoxOrbConfig
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-orb',
+    version='0.1',
+    description='A NetBox plugin for Managing Orb',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
